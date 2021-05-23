62fec98713b0303b6d32084ef329eddcdb761a43
Re9iNee <attarzadeh76@gmail.com>
2021-05-23 13:15:56 +0430

    Task 02 - Method 02 - Create Category

--- International Imamreza University /src/services/category.js
+++ International Imamreza University /src/services/category.js
@@ -1,5 +1,12 @@
 const {
-    normalizeQueryString
+    normalizeQueryString,
+    NotNullColumnsFilled,
+    checkDuplicate,
+    normalizeQueryString_Create,
+    sqlDate,
+    endIsLenghty,
+    getDate,
+    getTime
 } = require("../utils/commonModules");
 
 require("dotenv").config({
@@ -47,6 +54,83 @@ const ws_loadCategory = async (connection, filters = new Object(null), customQue
     }
 }
 
+
+const ws_createCategory = async (connection, details = new Object(null)) => {
+    // details are the parameters sent for creating table
+    const {
+        title
+    } = details;
+
+    // Not Null Values
+    // NOTE: "title", "dateCreated", "timeCreated", "favourite", "checked"
+    let notNullColumns = ["title"];
+    if (!NotNullColumnsFilled(details, ...notNullColumns))
+        return {
+            status: "Failed",
+            msg: "Fill Parameters Utterly",
+            details,
+            notNullColumns
+        }
+
+    // check for duplicates (unique Columns)
+    // NOTE: "title"
+    const duplicateUniqueTitle = await checkDuplicate(connection, {
+        title
+    }, ws_loadCategory);
+    if (duplicateUniqueTitle)
+        return {
+            status: "Failed",
+            msg: "Error Creating Row, Violation of unique values",
+            uniqueColumn: "title",
+            details
+        }
+
+
+    // NOTE: Auto Generate Date And Time
+    const d = new Date();
+    details.dateCreated = getDate(d);
+    details.timeCreated = getTime(d);
+
+    const {
+        pool,
+        poolConnect
+    } = connection;
+    // ensures that the pool has been created
+    await poolConnect;
+
+    let queryString = `INSERT INTO 
+    [${DB_DATABASE}].[dbo].[tblCategory] 
+    ($COLUMN)
+    VALUES ($VALUE);
+    SELECT SCOPE_IDENTITY() AS categoryId;`;
+    // normalizeQS_Create => (queryString, {title: "sth"}, ...configs)
+    queryString = normalizeQueryString_Create(queryString, details);
+    try {
+        const request = pool.request();
+        const result = await request.query(queryString);
+        const id = result.recordset[0].categoryId;
+        return id;
+    } catch (err) {
+        console.error("ws_createCategory SQL error: ", err);
+        return {
+            status: "Failed",
+            method: "ws_createCategory",
+            mgs: err
+        }
+    }
+}
+
+// TODO: for update method
+//  // check for custome Validation
+//     // NOTE: dateModified should be after dateCreated
+//     if ("dateModified" in details) {
+//         let start = new sqlDate(dateCreated.split('-'));
+//         let end = new sqlDate(dateModified.split('-'));
+//         let sth = endIsLenghty(start, end);
+//         debugger
+//     }
+
 module.exports = {
     ws_loadCategory,
+    ws_createCategory,
 }
