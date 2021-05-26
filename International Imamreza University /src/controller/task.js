ac796668de531026cec596309a61f9eef39fca38
Re9iNee <attarzadeh76@gmail.com>
2021-05-26 13:05:55 +0430

    Task 3 - Method 02 - Create in tblTask - Application CRUD

--- International Imamreza University /src/controller/task.js
+++ International Imamreza University /src/controller/task.js
@@ -1,5 +1,5 @@
 const {
-    ws_loadTask
+    ws_loadTask, ws_createTask
 } = require("../services/task");
 const {
     poolConnect,
@@ -14,6 +14,18 @@ exports.getTask = async (req, res) => {
         poolConnect
     }, query);
     // TODO: type validation
+    res.send({
+        result
+    })
+}
+
+exports.makeTask = async (req, res) => {
+    // T03 - Method 02
+    // Attach params to body as an JSON Format
+    const result = await ws_createTask({
+        pool,
+        poolConnect
+    }, req.body);
     res.send({
         result
     })
--- International Imamreza University /src/router/task.js
+++ International Imamreza University /src/router/task.js
@@ -4,5 +4,6 @@ const taskCtrlr = require("../controller/task");
 const router = express.Router();
 
 router.get("/task", taskCtrlr.getTask);
+router.post("/task", taskCtrlr.makeTask);
 
 module.exports = router;
